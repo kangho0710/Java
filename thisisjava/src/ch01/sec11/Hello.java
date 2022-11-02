7aa1c9e09db6f9951feb6d5fd1b62dbb7da82c47
schoolzonespeedrcer <choigo523@naver.com>
2022-11-02 16:48:07 +0900

    이것이_자바다 연습문제

---
+++ thisisjava/src/ch01/sec11/Hello.java
@@ -0,0 +1,20 @@
+package ch01.sec11;
+/** 반갑습니다
+ * 이것은 도큐먼트 주석
+ * @author use(작성자)
+ */
+
+ /*
+  장제목: 1장 자바 시작하기
+  그냥 주석
+  작성일 2022 .09.19
+  */
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello java");
+	}
+
+}
